2c0f91543255e76e80f117de6098175330ecdecc
Natallia <natalyasafarevich@gmail.com>
2025-11-15 09:22:35 +0100

    BS-2 feat: add basic footer component & footer links

---
+++ src/widgets/footer/consts.ts
@@ -0,0 +1,50 @@
+export const FOOTER_LINKS = [
+  {
+    title: 'Home',
+    links: [
+      {
+        label: 'Categories',
+        href: '/#categories',
+      },
+      {
+        label: 'Top Books',
+        href: '/#top-books',
+      },
+      {
+        label: 'Pricing',
+        href: '/#prices',
+      },
+    ],
+  },
+  {
+    title: 'Books',
+    links: [
+      {
+        label: 'Categories',
+        href: '/categories',
+      },
+
+      {
+        label: 'Novelty',
+        href: '/novelty',
+      },
+      {
+        label: 'Collection',
+        href: '/collection',
+      },
+      {
+        label: 'Search',
+        href: '/search',
+      },
+    ],
+  },
+  {
+    title: 'Subscription',
+    links: [
+      {
+        label: 'Plans',
+        href: '/subscriptions',
+      },
+    ],
+  },
+];
---
+++ src/widgets/footer/ui/Footer.tsx
@@ -0,0 +1,5 @@
+import s from './Footer.module.scss';
+
+export const Footer = () => {
+  return <footer className={s.footer}>Footer</footer>;
+};
