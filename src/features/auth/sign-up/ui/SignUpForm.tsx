82c443a237322aab2a4a0f1fbb2da53380a5381e
Natallya <59231303+natalyasafarevich@users.noreply.github.com>
2025-11-24 13:14:45 +0100

    BS-11 feat: Connected sign in form
    * BS-11 feat: delete unnessury code
    
    * BS-11 feat: make signWithEmail fc universal & add optional props: isRegistrition
    
    * BS-11 feat: add getLoggedInUser in auth layout

--- src/features/auth/sign-up/ui/SignUpForm.tsx
+++ src/features/auth/sign-up/ui/SignUpForm.tsx
@@ -5,10 +5,8 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { signUpSchema, SignUpSchemaData } from '@/shared/schemes/signUpSchema';
 import { Button } from '@/shared/ui/button/Button';
-import { signUpWithEmail } from '@/shared/actions/signUpWithEmail';
+import { signWithEmail } from '@/shared/actions/signWithEmail';
 import s from './SignUpForm.module.scss';
-import { useEffect } from 'react';
-import { getLoggedInUser } from '@/shared/module/appwrite';
 
 export const SignUpForm = () => {
   const {
@@ -23,16 +21,11 @@ export const SignUpForm = () => {
   const onSubmit = async (data: SignUpSchemaData) => {
     if (!data) return;
     try {
-      await signUpWithEmail(data);
-      getLoggedInUser().then((data) => {
-        console.log(data);
-      });
+      await signWithEmail(data, true);
     } catch (err) {
       console.error('Signup error:', err);
     }
   };
-
-  useEffect(() => {}, []);
 
   const isDisabled = !isValid;
 
--- src/shared/actions/signWithEmail.ts
+++ src/shared/actions/signWithEmail.ts
@@ -1,23 +1,27 @@
 'use server';
 
 import { ID } from 'node-appwrite';
-import { createAdminClient } from '../module/appwrite';
+import { createAdminClient, createSessionClient } from '../module/appwrite';
 import { cookies } from 'next/headers';
-import { SignUpSchemaData } from '../schemes/signUpSchema';
 
-export async function signUpWithEmail(formData: SignUpSchemaData) {
+type FormData = { email: string; password: string; name?: string };
+
+export async function signWithEmail(formData: FormData, isRegistration: boolean) {
   const email = formData.email;
   const password = formData.password;
   const name = formData.name;
 
   const { account } = await createAdminClient();
 
-  await account.create({
-    userId: ID.unique(),
-    email,
-    password,
-    name,
-  });
+  if (isRegistration) {
+    await account.create({
+      userId: ID.unique(),
+      email,
+      password,
+      name,
+    });
+  }
+
   const session = await account.createEmailPasswordSession({
     email,
     password,
