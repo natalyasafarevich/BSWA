2dc83ed0cd76083c95d0dd7735f239192f22eca4
Natallia <natalyasafarevich@gmail.com>
2025-12-01 14:45:29 +0100

    BS-14 feat: add user name & email in the user card

--- src/features/account/user-card/ui/UserCard.tsx
+++ src/features/account/user-card/ui/UserCard.tsx
@@ -7,8 +7,12 @@ import { selectInitialData } from '@/shared/slices/user/user-slice';
 export const UserCard = () => {
   const userData = useAppSelector(selectInitialData);
   return (
-    <div>
+    <div className={s.row}>
       <UserPhoto src="/test-user-icon.svg" alt="user-photo" />
+      <div className={s.box}>
+        <p className={s.name}>{userData.name}</p>
+        <p className={s.email}>{userData.email}</p>
+      </div>
     </div>
   );
 };
